fb2c33475409d6b7a564f5640afcf15700714a5f
Jen808 <145046398+Jen808@users.noreply.github.com>
2024-10-28 07:37:51 -0400

    Add files via upload

---
+++ zCopyFiles-HW01.bat
@@ -0,0 +1,6 @@
+echo off
+echo //***************************************************************
+echo // Homework 1 web application files
+echo //***************************************************************
+erase /q "C:\Program Files\Ampps\www\HW01\*.*"
+copy "C:\SSI\Colleges\UniversityOfMichigan\2024-T3-Fall-SI379\Assignments\zTries\HW01\*.*" "C:\Program Files\Ampps\www\HW01\*.*"
