0da8615e4f2f371e681a50c557d2141ed7bae605
Jennifer Jung <chorong@umich.edu>
2024-10-28 08:51:05 -0400

    done

--- zCopyFiles-HW01.bat
+++
@@ -1,6 +0,0 @@
-echo off
-echo //***************************************************************
-echo // Homework 1 web application files
-echo //***************************************************************
-erase /q "C:\Program Files\Ampps\www\HW01\*.*"
-copy "C:\SSI\Colleges\UniversityOfMichigan\2024-T3-Fall-SI379\Assignments\zTries\HW01\*.*" "C:\Program Files\Ampps\www\HW01\*.*"
---
+++ zCopyFiles-HW02.bat
@@ -0,0 +1,6 @@
+echo off
+echo //***************************************************************
+echo // Homework 1 web application files
+echo //***************************************************************
+erase /q "C:\Program Files\Ampps\www\HW02\*.*"
+copy "C:\SSI\Colleges\UniversityOfMichigan\2024-T3-Fall-SI379\Assignments\zTries\HW02\*.*" "C:\Program Files\Ampps\www\HW02\*.*"
